8baf78c49485c82e1cffc6823bedcaed12d97f20
tonipal <palmola.toni@gmail.com>
2022-11-21 15:32:58 +0200

    own middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log('Hello from the middleware 👋');
+    next(); 
+})
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString;
+    next();
+})
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 const getAllTours = (req, res) => {
