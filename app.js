49acafefa1e8a3f2c4cfed2baeee578e454a59f5
tonipal <palmola.toni@gmail.com>
2022-11-21 15:36:31 +0200

    missing parentheses

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString;
+    req.requestTime = new Date().toISOString();
     next();
 })
 
