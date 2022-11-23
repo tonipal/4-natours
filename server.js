921d85c5d6011caa011e2f81cefbdbafcdf14358
tonipal <86857995+tonipal@users.noreply.github.com>
2022-11-23 15:40:33 +0200

    mongoose schema and model testing

--- server.js
+++ server.js
@@ -15,11 +15,33 @@ mongoose
     .then(() => console.log('DB connection succesful!'));
 
 const tourSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
-    price: Number
-})
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+    name: 'The Forest Hiker',
+    rating: 4.7,
+    price: 497
+});
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('ERROR 💥:', err)
+});
 
 
 
