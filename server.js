9635e5a24495119fc31479b4aa3c193a8a0105cc
tonipal <86857995+tonipal@users.noreply.github.com>
2022-11-23 15:30:12 +0200

    prettier "error" -> "off"

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({path: './config.env'});
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('DB connection succesful!'));
+
+const tourSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    price: Number
+})
+
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
