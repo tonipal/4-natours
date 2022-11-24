e561a98f471b968dbd42560dfd8dacb14726c768
tonipal <86857995+tonipal@users.noreply.github.com>
2022-11-24 18:47:46 +0200

    CRUD operations using mongoose models

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,30 +24,44 @@ const Tour = require('./../models/tourModel');
 //     next();
 // };
 
-exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
 
-    res.status(200).json({
-        status: 'success',
-        // results: tours.length,
-        // data: {
-        //     tours: tours
-        // }
-    });
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+
 }
 
-exports.getTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
+exports.getTour = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id);
 
-    // const tour = tours.find(el => el.id === id)
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 
-    // res.status(200).json({
-    //     status: 'success',
-    //     data: {
-    //         tour
-    //     }
-    // });
+
 }
 
 exports.createTour = async (req, res) => {
