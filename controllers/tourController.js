9d09360ef9abb5d77b80363ce82036f89ee5c9c1
tonipal <palmola.toni@gmail.com>
2022-11-24 11:36:47 +0200

    deleted old code, changed port

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,15 +14,15 @@ const Tour = require('./../models/tourModel');
 //     next();
 // }
 
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price'
-        })
-    }
-    next();
-}
+// exports.checkBody = (req, res, next) => {
+//     if (!req.body.name || !req.body.price) {
+//         return res.status(400).json({
+//             status: 'fail',
+//             message: 'Missing name or price'
+//         })
+//     }
+//     next();
+// };
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -50,27 +50,22 @@ exports.getTour = (req, res) => {
     // });
 }
 
-exports.createTour = (req, res) => {
-    req.status(201).json({
-        status: 'success',
-        // data: {
-        //     tour: newTour
-        // }
-    });
-    // console.log(req.body);
+exports.createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
 
-    // const newId = tours[tours.length -1].id + 1;
-    // const newTour = Object.assign({id: newId}, req.body);
-
-    // tours.push(newTour);
-    // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    //     res.status(201).json({
-    //         status: 'success',
-    //         data: {
-    //             tour: newTour
-    //         }
-    //     })
-    // })
+        req.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
 
 exports.updateTour = (req, res) => {
@@ -80,7 +75,7 @@ exports.updateTour = (req, res) => {
         data: {
             tour: '<Updated tour here...>'
         }
-    })
+    });
 }
 
 exports.deleteTour = (req, res) => {
