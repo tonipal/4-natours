3e9a79e3d66c1ed614dec720e611f0e25c6c695d
tonipal <86857995+tonipal@users.noreply.github.com>
2022-11-24 20:08:23 +0200

    added remaining CRUD operations

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -82,20 +82,41 @@ exports.createTour = async (req, res) => {
     }
 }
 
-exports.updateTour = (req, res) => {
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    });
 }
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
+
+
 }
