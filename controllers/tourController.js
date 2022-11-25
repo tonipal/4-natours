13b0109b8b30c4a6ece05e739641e10b9e773ef2
tonipal <palmola.toni@gmail.com>
2022-11-25 12:18:45 +0200

    sorting, field limiting and pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,14 +12,31 @@ exports.getAllTours = async (req, res) => {
         // 1b) Advanced filtering
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
 
         let query = Tour.find(JSON.parse(queryStr));
 
         // 2) Sorting
         if(req.query.sort) {
-            query = query.sort(req.query.sort)
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt');
         }
+
+        // 3) Field limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+
+        // 4) Pagination
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page -1) * limit;
+
+        query = query.skip(skip).limit(limit);
 
         // EXECUTE QUERY
         const tours = await query;
