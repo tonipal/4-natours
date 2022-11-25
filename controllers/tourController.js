e95a0c0501c1f1af0d74292757cb04d009e0f9cd
tonipal <palmola.toni@gmail.com>
2022-11-25 08:14:43 +0200

    deleted commented code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,29 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-// const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-
-// exports.checkID = (req, res, next, val) => {
-//     console.log(`Tour id is: ${val}`);
-
-//     if (req.params.id * 1 > tours.length) {
-//         return res.status(404).json({
-//             status: 'fail',
-//             message:'Invalid ID'
-//         });
-//     }
-//     next();
-// }
-
-// exports.checkBody = (req, res, next) => {
-//     if (!req.body.name || !req.body.price) {
-//         return res.status(400).json({
-//             status: 'fail',
-//             message: 'Missing name or price'
-//         })
-//     }
-//     next();
-// };
-
 exports.getAllTours = async (req, res) => {
     try {
         const tours = await Tour.find();
