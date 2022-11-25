d68ee2bcada7eb4b18f3443b5957af7579f5b05e
tonipal <palmola.toni@gmail.com>
2022-11-25 14:04:10 +0200

    updated pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,6 +41,11 @@ exports.getAllTours = async (req, res) => {
 
         query = query.skip(skip).limit(limit);
 
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) throw new Error('This page does not exist!');
+        }
+
         // EXECUTE QUERY
         const tours = await query;
 
