4101757f7026066f6e8e2a493855ad4fad63f93c
tonipal <palmola.toni@gmail.com>
2022-11-25 14:23:50 +0200

    Top 5 implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,10 @@ const { Query } = require('mongoose');
 const Tour = require('./../models/tourModel');
 
 exports.aliasTopTours = (req, res, next) => {
-
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
 };
 
 exports.getAllTours = async (req, res) => {
