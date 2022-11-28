8351d2edf989adaceb0e34f01101972b0f285655
tonipal <palmola.toni@gmail.com>
2022-11-28 15:43:16 +0200

    Query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -53,7 +53,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -77,6 +81,21 @@ tourSchema.pre('save', function(next) {
 //     console.log(doc);
 //     next();
 // });
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+    this.find({ secretTour: { $ne: true } });
+
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+    console.log(docs);
+    next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
