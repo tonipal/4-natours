3ce8e072a34fa2aee7174f3727688d772454b81d
tonipal <palmola.toni@gmail.com>
2022-11-28 12:31:02 +0200

    Aggregation Pipeline: Matching and Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,7 +32,6 @@ exports.getAllTours = async (req, res) => {
             message: err
         });
     }
-
 }
 
 exports.getTour = async (req, res) => {
@@ -51,8 +50,6 @@ exports.getTour = async (req, res) => {
             message: err
         });
     }
-
-
 }
 
 exports.createTour = async (req, res) => {
@@ -91,7 +88,6 @@ exports.updateTour = async (req, res) => {
             message: err
         });
     }
-
 }
 
 exports.deleteTour = async (req, res) => {
@@ -108,6 +104,41 @@ exports.deleteTour = async (req, res) => {
             message: err
         })
     }
+}
 
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } }
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    numTours: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                }
+            },
+            {
+                $sort: { avgPrice: 1 }
+            }
+        ]);
 
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats
+            }
+        });
+
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
