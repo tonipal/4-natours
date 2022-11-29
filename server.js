880131393b01c967c2ed67496fe3c4c40b401b35
tonipal <palmola.toni@gmail.com>
2022-11-29 15:54:19 +0200

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 dotenv.config({path: './config.env'});
 const app = require('./app');
-
-
 
 const DB = process.env.DATABASE.replace(
     '<PASSWORD>',
@@ -24,9 +29,10 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-    console.log(err.name, err.message);
     console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
     server.close(() => {
         process.exit(1);
     });
 });
+
