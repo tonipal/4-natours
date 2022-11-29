34eb879c35fb980efa78cc7bb2afef8c9141950e
tonipal <palmola.toni@gmail.com>
2022-11-29 10:17:07 +0200

    formatting

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = require('./app');
 
 
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD);
 
 mongoose
     .connect(DB, {
