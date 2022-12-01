b05d00a0ebce99f5717fbf7137a62998ea2b0e45
tonipal <palmola.toni@gmail.com>
2022-12-01 09:22:57 +0200

    removed unused parameters

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,9 +18,9 @@ const handleValidationErrorDB = err => {
     return new AppError(message, 400);
 }
 
-const handleJWTError = err => new AppError('Invalid token. Please log in again!', 401);
+const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401);
 
-const handleJWTExpiredError = err => new AppError('Your token has expired! Please log in again.', 401);
+const handleJWTExpiredError = ()) => new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
@@ -65,8 +65,8 @@ module.exports = (err, req, res, next) => {
         if(error.name === 'CastError') error = handleCastErrorDB(error);
         if(error.code === 11000) error = handleDuplicateFieldsDB(error);
         if(error.name === 'ValidationError') error = handleValidationErrorDB(error);
-        if(error.name === 'JsonWebTokenError') error = handleJWTError(error);
-        if(error.name === 'TokenExpiredError') error = handleJWTExpiredError(error);
+        if(error.name === 'JsonWebTokenError') error = handleJWTError();
+        if(error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
 
         sendErrorProd(error, res);
