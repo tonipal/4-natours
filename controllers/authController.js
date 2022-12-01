635488bba5027973bdd7361a26f87328c63a8dd7
tonipal <palmola.toni@gmail.com>
2022-12-01 10:30:26 +0200

    Route protection

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,8 +66,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
     // 3) Check if user still exists
-    const freshUser = await User.findById(decoded.id);
-    if(!freshUser) {
+    const currentUser = await User.findById(decoded.id);
+    if(!currentUser) {
         return next(
             new AppError(
                 'The user belonging to this token no longer exists',
@@ -77,8 +77,13 @@ exports.protect = catchAsync(async (req, res, next) => {
     }
     
     // 4) Check if user changed password after the token was issued
-    
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError('User recently changed password! Please log in again.', 401)
+        );
+    }
 
-
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
     next();
 })
