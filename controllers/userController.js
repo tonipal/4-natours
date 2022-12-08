04a08325f198feded6fe976c90ed156bd889f6a9
tonipal <palmola.toni@gmail.com>
2022-12-08 08:21:49 +0200

    deleteMe to change current user active status

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,11 +50,20 @@ exports.updateMe = async (req, res, next) => {
 
 }
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+    await User.findByIdAndUpdate(req.user.id, { active: false })
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
+
 exports.createUser = (req, res) => {
     res.status(500).json({
         status: 'error',
         message: 'This route is not yet defined!'
-    })
+    });
 }
 
 exports.getUser = (req, res) => {
