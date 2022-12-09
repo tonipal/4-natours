33662f3182e5b9f5c87a3283ca036aa5be02ad73
tonipal <palmola.toni@gmail.com>
2022-12-09 13:15:26 +0200

    Populating Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -30,6 +30,19 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 });
 
+reviewSchema.pre(/^find/, function(next) {
+    this.populate({
+        path: 'tour',
+        select: 'name'
+    })
+    this.populate({
+        path: 'user',
+        select: 'name photo'
+    });
+    
+    next();
+})
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
